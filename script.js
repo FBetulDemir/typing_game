bdf108b2d05cd60e34a6faaf38c04b067e8bf852
FBetulDemir <fatmabetuld@gmail.com>
2024-04-17 09:40:20 +0200

    Fixed the style in index.css and added a list of words in script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const word = document.getElementById("word");
+
+
+const words = [
+    "dependent",
+    "dog",
+    "superficial",
+    "admit",
+    "juice",
+    "javascript",
+    "developer",
+    "airplane",
+    "great",
+    "fun",
+    "manipulate",
+    "cat",
+    "transition",
+    "school",
+    "computer",
+    "programming",
+    "drag",
+    "loving",
+    "north",
+]
